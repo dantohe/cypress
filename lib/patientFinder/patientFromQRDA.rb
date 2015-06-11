40c40436a88bb0368041419b710b7034ecd7226a
Daniel Antohe <dantohe@gmail.com>
2015-06-11 13:21:24 +0000

    Add unit tests for PatientFromQRDA -created a QRDA1 example that will be imported by the setup for unitTest patientFromQRDA_test -created 2 unitTests in patientFromQRDA_test

--- lib/patientFinder/patientFromQRDA.rb
+++ lib/patientFinder/patientFromQRDA.rb
@@ -1,6 +1,7 @@
 #a simple patient from QRDA - contains the source files name and the parsed record associated
 module PatientFinder
     class PatientFromQRDA
+    	
         #has a filename (the source file name) and a health-data-standards parsed record
         attr_accessor :fileName, :record
         def initialize(fileName, record)
